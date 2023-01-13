8a1407021fbeb739e7bc0810c456fd24b41f6261
rob grace <zwergie69@protonmail.com>
2023-01-13 12:27:45 +0100

    notes on my current understanding

--- projectOne/script.js
+++ projectOne/script.js
@@ -1,9 +1,15 @@
+
+
+
+// first chunk creates the message "p" - no idea what this other stuff is
+
 function createParagraph() {
     const para = document.createElement('p');
     para.textContent = 'you clicked the button';
     document.body.appendChild(para);
 
 }
+//assigning shit to the button 
 
 const buttons = document.querySelectorAll('button');
 
